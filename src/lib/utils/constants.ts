200d17617af267d0512ffa96a730d64d5ab19d0c
Felitendo <Felendo@web.de>
2024-07-20 20:53:49 +0200

    Removed Vendetta Proxy

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,7 +1,7 @@
 export const DISCORD_SERVER = "https://felo.gg/discord";
 export const GITHUB = "https://github.com/Felocord";
-export const PROXY_PREFIX = "https://bn-plugins.github.io/vd-proxy" || "https://vd-plugins.github.io/proxy";
-export const BUNNY_PROXY_PREFIX = "https://bn-plugins.github.io/vd-proxy" || "https://vd-plugins.github.io/proxy";
+export const PROXY_PREFIX = "https://bn-plugins.github.io/vd-proxy";
+export const BUNNY_PROXY_PREFIX = "https://bn-plugins.github.io/vd-proxy";
 export const HTTP_REGEX = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
 export const HTTP_REGEX_MULTI = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
 
