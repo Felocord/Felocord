99d3e45d8f17376c420a85e617332bd259738909
A1Asriel <27139654+A1Asriel@users.noreply.github.com>
2024-11-09 12:02:46 +0300

    chore: update GitHub link constant [skip ci] (#105)

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,5 +1,5 @@
 export const DISCORD_SERVER = "https://discord.gg/XjYgWXHb9Q";
-export const GITHUB = "https://github.com/pyoncord";
+export const GITHUB = "https://github.com/bunny-mod";
 export const HTTP_REGEX = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
 export const HTTP_REGEX_MULTI = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
 
