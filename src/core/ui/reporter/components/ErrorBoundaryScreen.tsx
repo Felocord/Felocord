5e3f9b72361159da57c072cafed44424184d57f5
pylixonly <82711525+pylixonly@users.noreply.github.com>
2024-11-09 17:03:06 +0800

    fix: wrap ErrorBoundaryScreen with SafeAreaProvider

--- src/core/ui/reporter/components/ErrorBoundaryScreen.tsx
+++ src/core/ui/reporter/components/ErrorBoundaryScreen.tsx
@@ -5,7 +5,7 @@ import { settings } from "@lib/api/settings";
 import { Codeblock, ErrorBoundary } from "@lib/ui/components";
 import { createStyles } from "@lib/ui/styles";
 import { tokens } from "@metro/common";
-import { Button, Card, SafeAreaView, Text } from "@metro/common/components";
+import { Button, Card, SafeAreaProvider, SafeAreaView, Text } from "@metro/common/components";
 import { ScrollView, View } from "react-native";
 
 import ErrorComponentStackCard from "./ErrorComponentStackCard";
@@ -31,22 +31,24 @@ export default function ErrorBoundaryScreen(props: {
     const debugInfo = getDebugInfo();
 
     return <ErrorBoundary>
-        <SafeAreaView style={styles.container}>
-            <View style={{ gap: 4 }}>
-                <Text variant="display-lg">Uh oh.</Text>
-                <Text variant="text-md/normal">A crash occurred while rendering a component. This could be caused by a plugin, Bunny or Discord itself.</Text>
-                <Text variant="text-sm/normal" color="text-muted">{debugInfo.os.name}; {debugInfo.discord.build} ({debugInfo.discord.version}); {debugInfo.bunny.version}</Text>
-            </View>
-            <ScrollView fadingEdgeLength={64} contentContainerStyle={{ gap: 12 }}>
-                <Codeblock selectable={true}>{props.error.message}</Codeblock>
-                {hasStack(props.error) && <ErrorStackCard error={props.error} />}
-                {isComponentStack(props.error) ? <ErrorComponentStackCard componentStack={props.error.componentStack} /> : null}
-            </ScrollView>
-            <Card style={{ gap: 6 }}>
-                <Button text="Reload Discord" onPress={() => BundleUpdaterManager.reload()} />
-                {!settings.safeMode?.enabled && <Button text="Reload in Safe Mode" onPress={() => toggleSafeMode()} />}
-                <Button variant="destructive" text="Retry Render" onPress={() => props.rerender()} />
-            </Card>
-        </SafeAreaView>
+        <SafeAreaProvider> {/* for iOS */}
+            <SafeAreaView style={styles.container}>
+                <View style={{ gap: 4 }}>
+                    <Text variant="display-lg">Uh oh.</Text>
+                    <Text variant="text-md/normal">A crash occurred while rendering a component. This could be caused by a plugin, Bunny or Discord itself.</Text>
+                    <Text variant="text-sm/normal" color="text-muted">{debugInfo.os.name}; {debugInfo.discord.build} ({debugInfo.discord.version}); {debugInfo.bunny.version}</Text>
+                </View>
+                <ScrollView fadingEdgeLength={64} contentContainerStyle={{ gap: 12 }}>
+                    <Codeblock selectable={true}>{props.error.message}</Codeblock>
+                    {hasStack(props.error) && <ErrorStackCard error={props.error} />}
+                    {isComponentStack(props.error) ? <ErrorComponentStackCard componentStack={props.error.componentStack} /> : null}
+                </ScrollView>
+                <Card style={{ gap: 6 }}>
+                    <Button text="Reload Discord" onPress={() => BundleUpdaterManager.reload()} />
+                    {!settings.safeMode?.enabled && <Button text="Reload in Safe Mode" onPress={() => toggleSafeMode()} />}
+                    <Button variant="destructive" text="Retry Render" onPress={() => props.rerender()} />
+                </Card>
+            </SafeAreaView>
+        </SafeAreaProvider>
     </ErrorBoundary>;
 }
